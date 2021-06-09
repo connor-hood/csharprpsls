eda145b46c54e4e1afe2521ebfd23719226559fd
Connor Hood <connor_hood@yahoo.com>
2021-06-09 12:47:33 -0500

    initial commit

---
+++ RPSLS/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSLS
+{
+    public class Game
+    {
+        //member variables
+        public string player;
+        //constructor
+        public Game()
+        {
+
+        }
+        
+        //member methods
+    }
+}
